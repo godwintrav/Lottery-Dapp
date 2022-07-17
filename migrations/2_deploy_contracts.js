bc1f1976d86e0d880db62bdf6a93e37ab1655ca0
Odenigbo Godwin <godwintrav@gmail.com>
2022-07-17 10:32:47 +0000

    updated migration file

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,5 @@
-const Migrations = artifacts.require("Migrations");
+const Lottery = artifacts.require("Lottery");
 
 module.exports = function (deployer) {
-  deployer.deploy(Migrations);
+  deployer.deploy(Lottery);
 };
